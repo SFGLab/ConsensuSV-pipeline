e8788facba638ea3c19501b83e65c97c848e62eb
Mateusz Chilinski <mateusz@chilinski.eu>
2021-09-07 10:47:48 +0000

    testing & training scenarios ready

--- run_consensusv.py
+++ run_consensusv.py
@@ -42,15 +42,19 @@ class Benchmark1000G(luigi.Task):
         run_command("python -u /tools/ConsensuSV-1.0/charles_filter_n.py -s HG00512,HG00513,HG00514,HG00731,HG00732,HG00733,NA19238,NA19239,NA19240 -o /tools/ConsensuSV-1.0/output/consensuSV__HG00512.vcf,/tools/ConsensuSV-1.0/output/consensuSV__HG00513.vcf,/tools/ConsensuSV-1.0/output/consensuSV__HG00514.vcf,/tools/ConsensuSV-1.0/output/consensuSV__HG00731.vcf,/tools/ConsensuSV-1.0/output/consensuSV__HG00732.vcf,/tools/ConsensuSV-1.0/output/consensuSV__HG00733.vcf,/tools/ConsensuSV-1.0/output/consensuSV__NA19238.vcf,/tools/ConsensuSV-1.0/output/consensuSV__NA19239.vcf,/tools/ConsensuSV-1.0/output/consensuSV__NA19240.vcf > benchmark.txt")
 
 class RunConsensuSV(luigi.Task):
+    file_name_1 = luigi.Parameter(default=None)
+    file_name_2 = luigi.Parameter(default=None)
+    sample_name = luigi.Parameter()
+    already_done = luigi.Parameter(default=False)
 
     def requires(self):
-        return [CallVariants(sample_name="HG00512", train_1000g=True)]
+        return [CallVariants(file_name_1=self.file_name_1, file_name_2=self.file_name_2, sample_name=self.sample_name, train_1000g=False)]
 
     def output(self):
-        return luigi.LocalTarget("/tools/ConsensuSV-1.0/1000g_illumina.model")
+        return luigi.LocalTarget("/tools/ConsensuSV-1.0/output/consensuSV__%s.vcf" % self.sample_name)
 
     def run(self):
-        run_command("python -u /tools/ConsensuSV-1.0/main.py -f /pipeline/ -s SRR590764 -mod 1000g_illumina.model")
+        run_command("python -u /tools/ConsensuSV-1.0/main.py -f /pipeline/ -s %s -c breakdancer,breakseq,cnvnator,delly,lumpy,manta,tardis,whamg -mod 1000g_illumina.model" % self.sample_name)
 
 if __name__ == '__main__':
     luigi.run()
--- test_run.sh
+++ test_run.sh
@@ -1,1 +1,1 @@
-python sv_callings.py CallVariants --file-name-1 /tools/SRR590764_1.filt.fastq --file-name-2 /tools/SRR590764_2.filt.fastq --sample-name SRR --local-scheduler
+python run_consensusv.py RunConsensuSV --file-name-1 /tools/ERR018471_1.filt.fastq --file-name-2 /tools/ERR018471_2.filt.fastq --sample-name ERR018471 --workers 4 --local-scheduler > test.log
