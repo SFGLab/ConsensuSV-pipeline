e244cd82cd9cb8969b9fc6399a352812311f0f0e
Mateusz Chilinski <mateusz@chilinski.eu>
2021-11-02 15:21:09 +0100

    qf

--- align_genome.py
+++ align_genome.py
@@ -37,7 +37,11 @@ class MergeFastq(luigi.Task):
                 command_to_use = "cat"
             run_command("%s %s > %s" % (command_to_use, files, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R1.fastq"))
         else:
-            run_command("cp %s %s" % (self.file_name_1, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R1.fastq"))
+            if(".gz" in self.file_name_1):
+                run_command("gunzip -k %s %s" % (self.file_name_1, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R1.fastq"))
+            else:
+                run_command("cp %s %s" % (self.file_name_1, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R1.fastq"))
+
         if(";" in self.file_name_2):
             files = " ".join(self.file_name_2.split(";"))
             if(".gz" in self.file_name_2):
@@ -46,7 +50,10 @@ class MergeFastq(luigi.Task):
                 command_to_use = "cat"
             run_command("%s %s > %s" % (command_to_use, files, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R2.fastq"))
         else:
-            run_command("cp %s %s" % (self.file_name_2, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R2.fastq"))
+            if(".gz" in self.file_name_2):
+                run_command("gunzip -k %s %s" % (self.file_name_2, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R1.fastq"))
+            else:
+                run_command("cp %s %s" % (self.file_name_2, self.working_dir+"/pipeline/"+self.sample_name+"/"+self.sample_name+"_R1.fastq"))
 
 class QCAnalysis(luigi.Task):
     working_dir = luigi.Parameter()
