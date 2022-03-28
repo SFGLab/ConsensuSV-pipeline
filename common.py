0c07660d6a129ba52c9773e661e00a125de29e9b
Mateusz Chilinski <mateusz@chilinski.eu>
2022-03-28 16:24:50 +0200

    string -> str

--- common.py
+++ common.py
@@ -5,11 +5,11 @@ def get_path_no_ext(path, extension_no=1):
     """Function for getting the path without extension.
 
     Args:
-        path (string): Path to the file.
+        path (str): Path to the file.
         extension_no (int, optional): Which extension should it stop on. E.g. for file sample.bam.bai, can return either "sample" (for extension_no=2) or "sample.bam" (for extension_no=1), depending on this parameter. Defaults to 1.
 
     Returns:
-        string: Modified path.
+        str: Modified path.
     """
     return ".".join(path.split(".")[0:-extension_no])
 
@@ -17,11 +17,11 @@ def get_path(path, extension_no=1):
     """Returns directory the file is in.
 
     Args:
-        path (string): Full path to the file.
+        path (str): Full path to the file.
         extension_no (int, optional): The directory to return - if 1, it returns file directory, if larget it returns the next scoped directory. Defaults to 1.
 
     Returns:
-        string: Modified path.
+        str: Modified path.
     """
     return "/".join(path.split("/")[0:-extension_no])+"/"
 
@@ -29,8 +29,8 @@ def run_command(command, conda_env=None):
     """Function for running CLI commands.
 
     Args:
-        command (string): Command to run.
-        conda_env (string, optional): Name of the conda environment to use. If None, runs default one. Defaults to None.
+        command (str): Command to run.
+        conda_env (str, optional): Name of the conda environment to use. If None, runs default one. Defaults to None.
     """
     if(conda_env):
         command = "bash -c \"source activate %s; %s\"" % (conda_env, command)
